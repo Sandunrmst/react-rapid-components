5f6bcd417bade0755b72b199e532fc5a729b56f0
sandunrmst <sandunalugc@gmail.com>
2023-12-26 14:52:24 +0530

    enable multi selection

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,28 +1,65 @@
-import { useState } from "react"
-import faqData from "./data"
-import './styles.css'
-
+import { useState } from "react";
+import faqData from "./data";
+import "./styles.css";
 
 const Faqs = () => {
-    const [selected, setSelected] = useState(null)
-    const singaleSelection = (getCurrentId)=>{
-        setSelected(getCurrentId === selected ? null : getCurrentId)
-    }
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multipleFaqs, setMultipleFaqs] = useState([]);
+
+  const handleSingaleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  };
+
+  const handleMultiselection = (getCurrentId) => {
+    let copyMultiple = [...multipleFaqs];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId)
+
+    console.log(findIndexOfCurrentId)
+    if(findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId)
+    else copyMultiple.splice(findIndexOfCurrentId, 1)
+
+    setMultipleFaqs(copyMultiple)
+  };
+
   return (
     <div className="wrapper">
-        <div className="faq">
-        { faqData && faqData.length > 0 ? faqData.map(item => <div key={item.id} className="item">
-            <div onClick={()=>singaleSelection(item.id)} className="title">
+      <button
+        className="btn"
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+      >
+        Enable Multi Selection
+      </button>
+      <div className="faq">
+        {faqData && faqData.length > 0 ? (
+          faqData.map((item) => (
+            <div key={item.id} className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiselection(item.id)
+                    : () => handleSingaleSelection(item.id)
+                }
+                className="title"
+              >
                 <h3>{item.question}</h3>
                 <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multipleFaqs.indexOf(item.id) !== -1 && (
+                    <div className="content">{item.answer}</div>
+                  )
+                : selected === item.id && (
+                    <div className="content">{item.answer}</div>
+                  )}
             </div>
-            {selected === item.id ? (<div className="content">{item.answer}</div>) : null}
-        </div>)
-        : <div> No data </div> 
-        }
-        </div>
+          ))
+        ) : (
+          <div> No data </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Faqs
+export default Faqs;
