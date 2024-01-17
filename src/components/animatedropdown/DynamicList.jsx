274aa441c9e7e7a64f28050d63dabde6b7abb6b0
sandunrmst <sandunalugc@gmail.com>
2024-01-17 13:25:56 +0530

    enable and disable animation

--- src/components/animatedropdown/DynamicList.jsx
+++ src/components/animatedropdown/DynamicList.jsx
@@ -4,7 +4,14 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 const DynamicList = () => {
   const [item, setItems] = useState([0, 1, 2, 3]);
 
-  const [animateParent] = useAutoAnimate();
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const [animateParent, setAnimateState] = useAutoAnimate();
+
+  function toggleAnimation() {
+    setAnimateState(!isEnabled);
+    setIsEnabled(!isEnabled);
+  }
 
   function add() {
     setItems([...item, item.length]);
@@ -18,12 +25,20 @@ const DynamicList = () => {
           <li key={item}>{item}</li>
         ))}
       </ul>
-      <button
-        onClick={add}
-        className="bg-red-700 p-3 rounded-md text-white font-semibold"
-      >
-        Add Number
-      </button>
+      <div className="flex justify-center gap-4 mt-2">
+        <button
+          onClick={add}
+          className="bg-red-700 p-3 rounded-md text-white font-semibold"
+        >
+          Add Number
+        </button>
+        <button
+          onClick={toggleAnimation}
+          className="bg-red-700 p-3 rounded-md text-white font-semibold"
+        >
+          {isEnabled ? "Disable" : "Enable"}
+        </button>
+      </div>
     </>
   );
 };
